afa1447a1427dadeee71420d1285367d65904d0d
ryougifujino <ryougifujino@gmail.com>
2019-07-17 23:54:58 +0800

    Update Functions

--- functions/functions.ts
+++ functions/functions.ts
@@ -12,3 +12,52 @@ function buildName(firstName = "Will", lastName: string) {
 // function buildName2(firstName?: string, lastName: string) {
 //     return firstName + " " + lastName;
 // }   // Error
+
+/** this */
+// this parameter
+interface Card {
+    suit: string;
+    card: number;
+}
+
+interface Deck {
+    suits: string[];
+    cards: number[];
+
+    createCardPicker(this: Deck): () => Card;
+}
+
+let deck: Deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    createCardPicker(this: Deck): () => Card {
+        // Now TypeScript knows that createCardPicker expects to be called on a Deck object. That
+        // means that this is of type Deck now, not any, so --noImplicitThis will not cause any
+        // errors.
+        return () => {
+            let pickedCard = Math.floor(Math.random() * 52);
+            let pickedSuit = Math.floor(pickedCard / 13);
+            return {suit: this.suits[pickedSuit], card: pickedCard % 13};
+        }
+    }
+};
+
+let cardPicker = deck.createCardPicker();
+let pickedCard = cardPicker();
+console.log("card: " + pickedCard.card + " of " + pickedCard.suit);
+
+// this parameters in callbacks
+interface UIElement {
+    addClickListener(onclick: (this: void, e: Event) => void): void;
+}
+
+/** Overloads */
+function foo(bar: boolean): number;
+function foo(bar: number): boolean;
+function foo(bar): number | boolean {
+    if (typeof bar === 'boolean') {
+        return 1;
+    } else if (typeof bar === 'number') {
+        return true;
+    }
+}
