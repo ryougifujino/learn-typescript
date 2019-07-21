cfa9a20718e15d50020b42763d99e450aa5140d3
ryougifujino <ryougifujino@gmail.com>
2019-07-22 03:08:23 +0800

    Update Enums

--- enums/enums.ts
+++ enums/enums.ts
@@ -57,4 +57,62 @@ let c: Circle = {
 };
 
 // ---
+enum E3 {
+    Foo,
+    Bar
+}
 
+function f(x: E3) {
+    // if (x !== E3.Foo || x !== E3.Bar) {  // Error
+    // }
+}
+
+/** Enums at runtime */
+enum E4 {
+    X
+}
+
+function f2(obj: { X: number }) {
+    return obj.X;
+}
+
+f2(E4);     // OK
+
+/** Enums at compile time */
+enum LogLevel {
+    ERROR, WARN, INFO, DEBUG
+}
+
+// type LogLevelStrings = 'ERROR' | 'WARN' | 'INFO' | 'DEBUG';
+type LogLevelStrings = keyof typeof LogLevel;
+let error: LogLevelStrings = 'ERROR';
+
+/** Reverse mappings */
+enum Enum {
+    A
+}
+
+let a = Enum.A;
+let nameOfA = Enum[a];
+// note string enum members do not get a reverse mapping generated at all
+
+/** const enums */
+
+// cannot have computed members
+const enum Directions {
+    Up = 1,
+    Down = Up * 2,
+    Left,
+    Right
+}
+
+let directions = [Directions.Up, Directions.Down, Directions.Left, Directions.Right];
+// generated code:
+// var directions = [0 /* Up */, 1 /* Down */, 2 /* Left */, 3 /* Right */];
+
+/** Ambient enums */
+declare enum Enum2 {
+    A = 1,
+    B,  // always considered computed
+    C = 2
+}
