6b6839da7da899a50b17bd5ea5cd771077669573
ryougifujino <ryougifujino@gmail.com>
2019-08-26 23:41:54 +0800

    Update Mapped Types

--- advanced-types/mapped-types.ts
+++ advanced-types/mapped-types.ts
@@ -65,3 +65,65 @@ type Flags2 = {
 // The compiler knows that it can copy all the existing property modifiers before adding any new ones.
 // For example, if Person.name was readonly, Partial<Person>.name would be readonly and optional.
 
+// proxify
+// 注意这里只是一个有get方法和set方法的对象的类型
+type Proxy<T> = {
+    get(): T
+    set(T): void
+}
+
+const exampleProxy: Proxy<string> = {
+    get() {
+        return 'STRING'
+    },
+    set(d: string) {
+    }
+};
+
+type Proxify<T> = {
+    [P in keyof T]: Proxy<T[P]>
+}
+
+function proxify<T>(o: T): Proxify<T> {
+    const newObj = {} as Proxify<T>;
+    for (let prop in o) {
+        if (o.hasOwnProperty(prop)) {
+            let value = o[prop];
+            newObj[prop] = {
+                get() {
+                    console.log("Handling something...");
+                    return value;
+                },
+                set(val: T) {
+                }
+            }
+        }
+    }
+    return newObj;
+}
+
+const proxyProps = proxify({
+    "A": 1,
+    "B": false,
+    1: "A"
+});
+
+console.log('proxyProps["A"]', proxyProps["A"].get());
+console.log('proxyProps["B"]', proxyProps["B"].get());
+console.log('proxyProps["1"]', proxyProps["1"].get());
+
+// unproxify
+function unproxify<T>(t: Proxify<T>): T {
+    let result = {} as T;
+    for (const k in t) {
+        if (t.hasOwnProperty(k)) {
+            result[k] = t[k].get();
+        }
+    }
+    return result;
+}
+
+const originalProps = unproxify(proxyProps);
+console.log("originalProps['A']", originalProps['A']);
+console.log("originalProps['B']", originalProps['B']);
+console.log("originalProps['1']", originalProps['1']);
