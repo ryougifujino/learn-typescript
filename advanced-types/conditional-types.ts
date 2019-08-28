e945b7367ca7b48933a5af6ea052e973e73ca143
ryougifujino <ryougifujino@gmail.com>
2019-08-28 23:56:20 +0800

    Add Distributive conditional types

--- advanced-types/conditional-types.ts
+++ advanced-types/conditional-types.ts
@@ -17,3 +17,12 @@ type T1 = TypeName<"a">;  // "string"
 type T2 = TypeName<true>;  // "boolean"
 type T3 = TypeName<() => void>;  // "function"
 type T4 = TypeName<string[]>;  // "object"
+
+// - Distributive conditional types
+// instantiation of T extends U ? X : Y with the type argument A | B | C for T is resolved as
+// (A extends U ? X : Y) | (B extends U ? X : Y) | (C extends U ? X : Y).
+type T10 = TypeName<string | (() => void)>;
+let t10: T10;
+let _t10: string | (() => void) = t10;
+type T12 = TypeName<string | string[] | undefined>;  // "string" | "object" | "undefined"
+type T11 = TypeName<string[] | number[]>;  // "object"
