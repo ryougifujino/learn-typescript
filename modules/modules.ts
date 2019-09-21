b9e5c945b38b57129cf0ab679ba9666534726c36
ryougifujino <ryougifujino@gmail.com>
2019-09-21 23:31:18 +0800

    Add Import

--- modules/modules.ts
+++ modules/modules.ts
@@ -5,3 +5,13 @@ export {ZipCodeValidator as RegExpBasedZipCodeValidator} from './ZipCodeValidato
 // 如同中转站，等于转发Export
 export * from "./StringValidator"; // exports 'StringValidator' interface
 export * from "./ZipCodeValidator";  // exports 'ZipCodeValidator' and const 'numberRegexp' class
+
+/** Import */
+// Import the entire module into a single variable
+import * as validator from './ZipCodeValidator';
+
+validator.numberRegexp.test('0');
+const zipCodeValidator = new validator.ZipCodeValidator();
+zipCodeValidator.isAcceptable('0');
+
+/** export = and import = require() */
