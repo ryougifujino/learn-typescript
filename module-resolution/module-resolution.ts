8185cf390c6f9425ba2be645add0b6d67f761591
ryougifujino <ryougifujino@gmail.com>
2019-10-03 23:14:58 +0800

    Add Module Resolution Strategies - Classic

--- module-resolution/module-resolution.ts
+++ module-resolution/module-resolution.ts
@@ -16,3 +16,24 @@ import { Component } from "@angular/core";
 */
 
 /** Module Resolution Strategies */
+// - Classic
+// relative import
+// import { b } from "./moduleB" in source file /root/src/folder/A.ts:
+// 1. /root/src/folder/moduleB.ts
+// 2. /root/src/folder/moduleB.d.ts
+
+// non-relative module imports
+// the compiler walks up the directory tree starting with the directory containing the importing
+// file, trying to locate a matching definition file.
+// import { b } from "moduleB", in a source file /root/src/folder/A.ts:
+// 1. /root/src/folder/moduleB.ts
+// 2. /root/src/folder/moduleB.d.ts
+// 3. /root/src/moduleB.ts
+// 4. /root/src/moduleB.d.ts
+// 5. /root/moduleB.ts
+// 6. /root/moduleB.d.ts
+// 7. /moduleB.ts
+// 8. /moduleB.d.ts
+
+// - Node
+// How Node.js resolves modules
