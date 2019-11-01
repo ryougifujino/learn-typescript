5799840b4fc6c2e79522dc048ffb6b1ea3756adf
ryougifujino <ryougifujino@gmail.com>
2019-11-01 23:52:25 +0800

    Add Merging Namespaces

--- decleration-merging/decleration-merging.ts
+++ decleration-merging/decleration-merging.ts
@@ -64,3 +64,37 @@ interface Document {
     createElement(tagName: string): HTMLElement;
     createElement(tagName: any): Element;
 }
+
+/** Merging Namespaces */
+namespace Animals {
+    export class Zebra { }
+}
+
+namespace Animals {
+    export interface Legged { numberOfLegs: number; }
+    export class Dog { }
+}
+// <=>
+/*
+namespace Animals {
+    export interface Legged { numberOfLegs: number; }
+
+    export class Zebra { }
+    export class Dog { }
+}
+*/
+
+// Non-exported members are only visible in the original (un-merged) namespace.
+namespace Animal {
+    let haveMuscles = true;
+
+    export function animalsHaveMuscles() {
+        return haveMuscles;
+    }
+}
+
+namespace Animal {
+    export function doAnimalsHaveMuscles() {
+        // return haveMuscles;  // Error, because haveMuscles is not accessible here
+    }
+}
