903270a6d813e516a0873576d765d5e057ad45e1
ryougifujino <ryougifujino@gmail.com>
2019-11-09 01:40:13 +0800

    Add The as operator

---
+++ jsx/jsx.ts
@@ -0,0 +1,18 @@
+/** Basic usage */
+// In order to use JSX you must do two things:
+// 1. Name your files with a .tsx extension
+// 2. Enable the jsx option
+/*
+Mode	        Input	    Output	                    Output File Extension
+preserve	    <div />	    <div />	                    .jsx
+react	        <div />	    React.createElement("div")	.js
+react-native	<div />	    <div />	                    .js
+*/
+
+/** The as operator */
+// TypeScript disallows angle bracket type assertions in .tsx files.
+/*
+// in .tsx
+var foo = <foo>bar;     // no
+var foo = bar as foo;   // yes
+*/
