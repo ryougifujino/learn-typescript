83a4144509321447b3ba6f7ad9e1d9b5f5932c63
ryougifujino <ryougifujino@gmail.com>
2019-11-10 01:55:19 +0800

    Add Type Checking - Value-based elements

--- jsx/jsx.tsx
+++ jsx/jsx.tsx
@@ -29,3 +29,65 @@ declare namespace JSX {
 }
 <foo />;    // ok
 <bar />;    // error
+
+// - Value-based elements
+/*
+import MyComponent from "./myComponent";
+
+<MyComponent />; // ok
+<SomeOtherComponent />; // error
+*/
+// There are two ways to define a value-based element:
+// 1. Function Component (FC)
+// 2. Class Component
+
+// Function Component
+interface FooProp {
+    name: string;
+    X: number;
+    Y: number;
+}
+
+declare function AnotherComponent(prop: { name: string });
+function ComponentFoo(prop: FooProp) {
+    // TS enforces that its return type must be assignable to JSX.Element
+    return <AnotherComponent name={prop.name} />;
+}
+
+// Class Component
+// The element instance type must be assignable to JSX.ElementClass or it will result in an error.
+declare namespace JSX {
+    interface ElementClass {
+        render: any;
+    }
+}
+
+class MyComponent {
+    render() {}
+}
+function MyFactoryFunction() {
+    return { render: () => {} }
+}
+
+<MyComponent />; // ok
+<MyFactoryFunction />; // ok
+
+class NotAValidComponent {}
+function NotAValidFactoryFunction() {
+    return {};
+}
+
+<NotAValidComponent />; // error
+<NotAValidFactoryFunction />; // error
+
+// - Attribute type checking
+
+// - Children type checking
+
+/** The JSX result type */
+
+/** Embedding Expressions */
+
+/** React  integration */
+
+/** Factory Functions */
