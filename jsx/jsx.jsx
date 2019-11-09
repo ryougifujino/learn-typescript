31ecea8c662d92550e05f5eeba45188396afbd5e
ryougifujino <ryougifujino@gmail.com>
2019-11-09 22:56:36 +0800

    Add Type Checking - Intrinsic elements

--- jsx/jsx.jsx
+++ jsx/jsx.jsx
@@ -8,7 +8,6 @@ preserve	    <div />	    <div />	                    .jsx
 react	        <div />	    React.createElement("div")	.js
 react-native	<div />	    <div />	                    .js
 */
-
 /** The as operator */
 // TypeScript disallows angle bracket type assertions in .tsx files.
 /*
@@ -16,3 +15,7 @@ react-native	<div />	    <div />	                    .js
 var foo = <foo>bar;     // no
 var foo = bar as foo;   // yes
 */
+/** Type Checking */
+// An intrinsic element always begins with a lowercase letter, and a value-based element always
+// begins with an uppercase letter.
+// Intrinsic elements
---
+++ jsx/jsx.tsx
@@ -0,0 +1,31 @@
+/** Basic usage */
+// In order to use JSX you must do two things:
+// 1. Name your files with a .tsx extension
+// 2. Enable the jsx option
+/*
+Mode	        Input	    Output	                    Output File Extension
+preserve	    <div />	    <div />	                    .jsx
+react	        <div />	    React.createElement("div")	.js
+react-native	<div />	    <div />	                    .js
+*/
+
+/** The as operator */
+// TypeScript disallows angle bracket type assertions in .tsx files.
+/*
+// in .tsx
+var foo = <foo>bar;     // no
+var foo = bar as foo;   // yes
+*/
+
+/** Type Checking */
+// An intrinsic element always begins with a lowercase letter, and a value-based element always
+// begins with an uppercase letter.
+
+// - Intrinsic elements
+declare namespace JSX {
+    interface IntrinsicElements {
+        foo: any;
+    }
+}
+<foo />;    // ok
+<bar />;    // error
