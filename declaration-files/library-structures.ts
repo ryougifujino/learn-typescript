e0d95201eb105b5ad5f788e345802b8e95c324f9
ryougifujino <ryougifujino@gmail.com>
2019-11-20 23:55:36 +0800

    Add Identifying Kinds of Libraries - Module Plugin or UMD Plugin

--- declaration-files/library-structures.ts
+++ declaration-files/library-structures.ts
@@ -53,6 +53,31 @@ They will rarely have:
 */
 
 // - Module Plugin or UMD Plugin
+/*
+import * as m from 'someModule';
+
+// You can also import other modules if needed
+import * as other from 'anotherModule';
+
+// Here, declare the same module as the one you imported above
+declare module 'someModule' {
+    // Inside, add new function, classes, or variables. You can use
+    // unexported types from the original module if needed.
+    export function theNewMethod(x: m.foo): other.bar;
+
+    // You can also add new properties to existing interfaces from
+    // the original module by writing interface augmentations
+    export interface SomeModuleOptions {
+        someModuleSetting?: string;
+    }
+
+    // New types can also be declared and will appear as if they
+    // are in the original module
+    export interface MyModulePluginOptions {
+        size: number;
+    }
+}
+*/
 
 // - Global Plugin
 
